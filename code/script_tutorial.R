0a136bdf74415320f0cf6ac700c911662f07121c
ohora-942329 <o_hora@uncg.edu>
2025-08-26 10:00:49 -0400

    edit. script tutorial

--- code/script_tutorial.R
+++ code/script_tutorial.R
@@ -12,3 +12,26 @@ mean(x)
 
 ## estimate SD
 sd(x)
+
+z <- c(1, 2, 3)
+z
+## estimate mean
+
+# Exercercise ----------------------------------------------------------------
+##create a vector with length 10 assign it to z
+Z<-c(1:10)
+z<-seq(1,10, length = 10)
+z<-letters[1:10]
+# create a numeric matrix with two rows x 2 columns, assign to m
+m<- matrix(1: 4, nrow = 2, ncol = 2)
+m<-cbind(c(1, 2), c(3,4))
+m<-rbind(c(1, 2), c(3, 4))
+
+#data frame
+
+df0<- data.frame(name = c("smith", "John", "kate", "Akira"), height = c(154, 170, 156, 175))
+
+# Example code ------------------------------------------------------------
+
+
+
