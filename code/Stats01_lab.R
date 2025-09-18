12d728438f6c08b20a39e82ae4861dd128a75636
obssihora <o_hora@uncg.edu>
2025-09-18 09:31:41 -0400

    Update Stats01_lab.R

--- code/Stats01_lab.R
+++ code/Stats01_lab.R
@@ -1,57 +1,91 @@
 library(tidyverse)
+library(patchwork)
+
+# central tendency  -------------------------------------------------------
+
 #Create a new vector z with length  
 #1000 as exp(rnorm(n = 1000, mean = 0, sd = 0.1)), and calculate the arithmetic mean, geometric mean, and median.
 
 z<-exp(rnorm(n = 1000, mean = 0, sd = 0.1))
 
 #Q1
+# arithmetic mean
+## using a built-in function
 
-mean(z)
+mu_z <- mean(z)
+
+
+## manual
+sum(z) / length(z)
 n_z <- length(z)
-sum_z <- sum(z)
+mu_z <- mu_z / n_z
+
+
 # Geometric mean
-mu_z <- sum_z / n_z
-exp(mean(log(z)))
-print(mu_z)
+## taking the mean in log scale
+mug_z <- exp(mean(log(z)))
+## use prod() function
+prod(z)^(1 / length(z))
+
+# median
+## built-in function
 med_z<- median(z)
+## manual (even number, so get two mid-point values)
+z <- sort(z)
+id1 <- length(z) / 2
+id2 <- (length(z) + 1) / 2
+0.5 * sum(c(z[id1], z[id2]))
 
 #Q2
 #Draw a histogram of z using functions tibble(), ggplot(), and geom_histogram().
 
 df_z<-tibble(x=z)
-z %>% 
-  tibble() %>% 
-  ggplot(aes(x=z))+
-  geom_histogram()
+g1 <- df_z %>% 
+  ggplot(aes(x = x)) +
+  geom_histogram() 
+  
 #Q3
 #Draw vertical lines of arithmetic mean, geometric mean, and 
 #median on the histogram with different colors using a function geom_vline().
-z %>% 
-  tibble() %>% 
-  ggplot(aes(x=z))+
-  geom_histogram()+
-geom_vline(xintercept = mu_z,
-           color= "salmon")
-  
-#Q5
-#new vector 
+g1 <- df_z %>% 
+  ggplot(aes(x = x)) +
+  geom_histogram() +
+  geom_vline(xintercept = mu_z) +
+  geom_vline(xintercept = mug_z,
+             color = "red") +
+  geom_vline(xintercept = med_z,
+             color = "green")
+
+#Q5; Create a new vector z_rev as -z + max(z) + 0.5, and repeat step 1 ??? 4.
+ 
 z_rev<-z + max(z) + 0.1
-#get mean value
-mu_z_rev<-mean(z_rev)
-mug_z_rev<-exp(mean(log(z_rev)))
-med_z_rev<-median(z_rev)
-##re-define df_z
-df_z<-df_z %>% 
-  mutate(y=z_rev)
-##vistualization 
+
+## get mean values
+mu_z_rev <- mean(z_rev)
+mug_z_rev <- exp(mean(log(z_rev)))
+med_z_rev <- median(z_rev)
+
+## re-define df_z
+df_z <- df_z %>% 
+  mutate(y = z_rev)
+
+##visualization 
 g2<-df_z %>% 
   ggplot(aes(x=y))+
   geom_histogram()+
+geom_vline(aes(xintercept = mu_z_rev),
+           color= "red")+
+  geom_vline(xintercept = mu_z_rev, color = "green")+
+  geom_vline(xintercept = med_z_rev, color = "purple")
 
-# Variation ---------------------------------------------------------------
+# this syntax works for ggplot objects after loading `library(patchwork)`
+g1 / g2
+
+  
+  # Variation ---------------------------------------------------------------
 w <- rnorm(100, mean = 10, sd = 1)
 head(w) # show first 10 elements in w
-#gram to miligram
+#gram to milligram
 m<-1000
 #q2
 s2_w<- sum(w-mean(w))^2/length(w)
@@ -59,7 +93,7 @@ s_w<-sqrt(s2_w)
 s2_m<-sum(m-mean(m))^2/length(m)
 s_m<-sqrt(s2_m)
 ##MAD
-mad_w<-median(abs(w-medaian(w)))
+mad_w<-median(abs(w-median(w)))
 mad_m<-median(abs(m-median(m)))
 #Q3 CV
 cv_w<-s_w/mean(w)
@@ -69,4 +103,4 @@ cv_m<-s_m/mean(m)
 
 
 
-  
+
