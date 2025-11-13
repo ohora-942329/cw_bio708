a241bda1fb3ed7d471673b4d69d1a2fea8b3a54f
obssihora <o_hora@uncg.edu>
2025-11-13 10:27:18 -0500

    Create stats10.R

---
+++ code/stats10.R
@@ -0,0 +1,96 @@
+#ctrl (command) +shift + N is a hotkey for creating a new file
+#ctrl + I is fix indent
+##Likelihood lab
+pacman::p_load(tidyverse, 
+               pactchwork,here)
+
+
+# R squar -----------------------------------------------------------------
+
+
+set.seed(1) # for reproducibility
+
+# hypothetical sample size
+n <- 100
+
+# true intercept and slope
+b <- c(0.1, 0.5)
+
+# hypothetical explanatory variable
+x1 <- rnorm(n = n, mean = 0, sd = 1)
+
+# create a design matrix
+X <- model.matrix(~x1)
+
+# expected values of y is a function of x
+# %*% means matrix multiplication
+# y = X %*% b equals y = b[1] + b[2] * x
+# recall linear algebra
+y_hat <- X %*% b
+
+# add normal errors
+y <- rnorm(n = n, mean = y_hat, sd = 0.5)
+
+# plot
+df0 <- tibble(y = y, x1 = x1)
+
+df0 %>% 
+  ggplot(aes(y = y,
+             x = x1)) + 
+  geom_point()
+
+## Develop a model used to generate the data
+m1 <- lm(y ~ x1, data = df0)
+summary(m1)
+df0 <- df0 %>% 
+  mutate(x2 = rnorm(n))
+
+df0 %>% 
+  ggplot(aes(x= x2,
+             y= y))+
+  geom_point()
+
+m2 <- lm(y ~ x1 + x2, data = df0)
+summary(m2)
+
+m1 <- lm(y ~ x1, data = df0)
+summary(m1)
+
+m2 <- lm(y ~ x1 +x2, data = df0)
+summary(m2)
+
+m3 <- lm(y ~ x2, data = df0)
+summary(m3)
+
+
+# Likelihood ratio test ---------------------------------------------------
+m1 <- lm(y ~ x1, data = df0)
+summary(m1)
+
+m2 <- lm(y ~ x1 +x2, data = df0)
+summary(m2)
+## deviance
+dev_m1<- -2*logLik(m1)
+dev_m2<- -2*logLik(m2)
+log_lr<- dev_m1-dev_m2
+summary(log_lr)
+
+anova(m1, m2, test = "Chisq")
+
+# AIC ---------------------------------------------------------------------
+
+
+AIC (m1)
+AIC (m2)
+## multiple model for 
+
+install.packages(MuMIn)
+
+url <- "https://raw.githubusercontent.com/aterui/public-proj_fish-richness-shubuto/master/data/data_vpart.csv"
+df_fish <- read_csv(url)
+
+m_full<- glm(n_sp~ distance + cat_area +hull_area,
+    data = df_fish,
+    family = "poisson")
+## magic spell 
+
